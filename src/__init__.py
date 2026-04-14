1e73b97982d4c5de9868c1ffc35751c50e55a69d
Maximilian Harrisson <maximilianharrisson@gmail.com>
2026-04-14 11:50:55 -0300

    Remove unused thermometer

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from .feature_extractor import FeatureExtractor
-from .thermometer import Thermometer
 from .mih import MIH
 from .iris_matcher import IrisMatcher
 
-__all__ = ['FeatureExtractor', 'Thermometer', 'MIH', 'IrisMatcher']
+__all__ = ['FeatureExtractor', 'MIH', 'IrisMatcher']
