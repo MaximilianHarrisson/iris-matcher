abbb60592dff8e0356e3e4317042476347b95473
Maximilian Harrisson <maximilianharrisson@gmail.com>
2026-06-04 18:02:33 -0300

    Register CNN feature extractors

--- src/__init__.py
+++ src/__init__.py
@@ -1,24 +1,32 @@
 from .biometry_extractor import BiometryExtractor
+from .convnext_extractor import ConvNextExtractor
 from .evaluator import Evaluator
 from .faiss_mih import FaissMIH
 from .feature_extractor import FeatureExtractor
+from .inceptionv3_extractor import InceptionV3Extractor
 from .iris_entry import IrisEntry
 from .iris_dataset import IrisDataset
 from .iris_matcher import IrisMatcher
 from .median_binarizer import MedianBinarizer
 from .mih import MIH
+from .mobilenetv2_extractor import MobileNetV2Extractor
+from .resnet50_extractor import Resnet50Extractor
 from .thermometer_binarizer import ThermometerBinarizer
 from .vgg16_extractor import Vgg16Extractor
 
 __all__ = [
     'BiometryExtractor',
+    'ConvNextExtractor',
     'Evaluator',
     'FaissMIH',
     'FeatureExtractor',
+    'InceptionV3Extractor',
     'IrisEntry', 'IrisDataset',
     'IrisMatcher',
     'MedianBinarizer',
     'MIH',
+    'MobileNetV2Extractor',
+    'Resnet50Extractor',
     'ThermometerBinarizer',
     'Vgg16Extractor',
 ]
